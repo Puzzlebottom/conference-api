dc58e81c9a05dd915d19be675bc447efa73adcc4
Michael Lawrie <michael@michaellawrie.com>
2021-05-24 10:23:25 -0500

    Refactors integration test

--- app.spec.js
+++ app.spec.js
@@ -3,22 +3,31 @@ import request from "supertest";
 import {app} from "./app.js";
 import {talkRepository} from "./talkRepository.js";
 
+const expectAction = (action) => ({
+  changesCountOf: async (valueAccessor, by) => {
+    const initialValue = await valueAccessor();
+    await action();
+    const endingValue = await valueAccessor();
+    expect(endingValue - initialValue).to.eql(by);
+  }
+});
+
+const talkCount = async () => (await talkRepository.findAllBySessionId(1)).length;
+
 describe('app', () => {
   describe('POST /api/talks', () => {
-
     it('should save a talk', async () => {
-      const currentNumberOfTalks = (await talkRepository.findAllBySessionId(1)).length;
-      await request(app)
+      const saveTalk = () => request(app)
         .post('/api/talks')
         .set('Accept', 'application/json')
         .send({title: 'john', duration: '5', sessionId: '1'})
         .expect(200);
-      const newCount = (await talkRepository.findAllBySessionId(1)).length;
-      expect(newCount - currentNumberOfTalks).to.eql(1);
+
+      await expectAction(saveTalk).changesCountOf(talkCount, 1);
     });
 
     it('should not save an invalid talk', async () => {
-       return request(app)
+      return request(app)
         .post('/api/talks')
         .set('Accept', 'application/json')
         .send({title: 'john', duration: 'foo', sessionId: '1'})
